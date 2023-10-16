68321b64fdd5697cb3dce0db1389667095f07c9d
SAE1\1012580 <1012580@student.sae.edu.au>
2023-10-16 12:11:56 +1000

    first push, got movement somewhat working

---
+++ Assets/Scripts/Rdean Scripts/clowncarnival_clown.cs
@@ -0,0 +1,25 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+
+public class clowncarnival_shooting : MonoBehaviour
+{
+
+   
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+    }
+}
---
+++ Assets/Scripts/Rdean Scripts/clowncarnival_playerstats.cs
@@ -0,0 +1,24 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class clowncarnival_playerstats : MonoBehaviour
+{
+
+    public int playerHealth = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    
+    }
+}
